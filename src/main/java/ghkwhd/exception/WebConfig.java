285ee28c8025303129b2aa761a8cf9d2a9f89510
HwaJong <hwajong1016@gmail.com>
2023-01-23 15:59:44 +0900

    API 예외 처리

--- src/main/java/ghkwhd/exception/WebConfig.java
+++ src/main/java/ghkwhd/exception/WebConfig.java
@@ -2,18 +2,28 @@ package ghkwhd.exception;
 
 import ghkwhd.exception.filter.Logfilter;
 import ghkwhd.exception.interceptor.LogInterceptor;
+import ghkwhd.exception.resolver.MyHandlerExceptionResolver;
+import ghkwhd.exception.resolver.UserHandlerExceptionResolver;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+
+    @Override
+    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        resolvers.add(new MyHandlerExceptionResolver());
+        resolvers.add(new UserHandlerExceptionResolver());
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
